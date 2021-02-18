6c16d8c69034c6a36d7ab9f0732f4d8e62d84409
Siritos <bax_91@mail.ru>
2021-02-18 23:02:47 +0300

    preprdo

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ $(document).ready(() => {
     autoplay: true,
     autoplaySpeed: 6000
   })
+  createSlides()
   $('#comments').slick({
     centerMode: false,
     adaptiveHeight: false,
@@ -62,5 +63,30 @@ const ViewportHeight = () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+  })
+}
+
+const createSlides = () => {
+  const slides = [
+    '1.png',
+    '2.png',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg'
+  ]
+
+  const commentDom = $('#comments')
+
+  slides.forEach((slide) => {
+    const newEl = `<div class="px-2 pb-5 w-5/6">
+     <div class="bg-white  rounded-lg  px-4 py-2  shadow-lg divide-y-2 divide-dashed divide-gray-300">
+      <img src="./src/assets/feedback/${slide}" alt="">
+      </div>
+      </div>`
+
+    commentDom.append(newEl)
   })
 }
